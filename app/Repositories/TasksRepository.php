fcdd2fe2423751c5dedb615d44ef6cfbe0aa9b02
Gatis Ķevans <gatis.kevans@gmail.com>
2021-10-07 01:31:42 +0300

    ToDo Tasks With MySQL Implementation

--- app/Repositories/TasksRepository.php
+++ app/Repositories/TasksRepository.php
@@ -8,7 +8,7 @@
     interface TasksRepository
     {
         public function fetchAllRecords(): TasksCollection;
-//        public function save(Record $record): void;
-//        public function getOne(string $id): ?Record;
-//        public function delete(Record $record): void;
+        public function save(Record $record): void;
+        public function getOne(string $id): ?Record;
+        public function delete(Record $record): void;
     }
